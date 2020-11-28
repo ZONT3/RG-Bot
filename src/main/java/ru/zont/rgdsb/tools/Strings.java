315ac7c5fd07e177292a11159b9e38a1539c7a25
ZONT_ <danya10001@gmail.com>
2020-11-29 02:20:00 +0300

    Правка счисляемых существительных

--- src/main/java/ru/zont/rgdsb/tools/Strings.java
+++ src/main/java/ru/zont/rgdsb/tools/Strings.java
@@ -8,17 +8,20 @@ public class Strings {
     public static ResourceBundle STR = ResourceBundle.getBundle("strings", new UTF8Control());
 
     public static String countPlayers(int count) {
-        return getPlural(count, STR.getString("plurals.players.few"), STR.getString("plurals.players.other"));
+        return getPlural(count, STR.getString("plurals.players.other"), STR.getString("plurals.players.few"), STR.getString("plurals.players.other"));
     }
 
     public static String countGMs(int count) {
-        return getPlural(count, STR.getString("plurals.gms.few"), STR.getString("plurals.gms.other"));
+        return getPlural(count, STR.getString("plurals.gms.one"), STR.getString("plurals.gms.few"), STR.getString("plurals.gms.other"));
     }
 
-    public static String getPlural(int count, String few, String other) {
+    public static String getPlural(int count, String one, String few, String other) {
         int ccount =(count % 100);
-        if ((ccount < 10 || ccount > 20) && ccount % 10 >= 2 && ccount % 10 <= 4)
+
+        if (ccount == 1 || (ccount > 20 && ccount % 10 == 1))
+            return String.format(one, count);
+       if ((ccount < 10 || ccount > 20) && ccount % 10 >= 2 && ccount % 10 <= 4)
             return String.format(few, count);
-        else return String.format(other, count);
+       return String.format(other, count);
     }
 }
