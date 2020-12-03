3120e83b83f77f06bb1d63c611ae9e07702e4f70
ZONT_ <danya10001@gmail.com>
2020-12-03 22:38:00 +0300

    Начало работы над самой командой

--- src/main/java/ru/zont/rgdsb/command/Exec.java
+++ src/main/java/ru/zont/rgdsb/command/Exec.java
@@ -2,8 +2,12 @@ package ru.zont.rgdsb.command;
 
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import org.jetbrains.annotations.NotNull;
+import ru.zont.rgdsb.tools.Commands;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.util.Properties;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static ru.zont.rgdsb.tools.Strings.STR;
 
@@ -14,7 +18,12 @@ public class Exec extends CommandAdapter {
 
     @Override
     public void onRequest(@NotNull MessageReceivedEvent event) throws UserInvalidArgumentException {
-
+        String input = Commands.parseInputRaw(this, event);
+        Pattern pattern = Pattern.compile("[^\\w]*```(java|python)\\n((.|\\n)+)```[^\\w]*");
+        Matcher matcher = pattern.matcher(input);
+        if (matcher.find())
+            event.getChannel().sendMessage(String.format("1: %s\n2: %s", matcher.group(1), matcher.group(2))).queue();
+        else throw new NotImplementedException();
     }
 
     @Override
