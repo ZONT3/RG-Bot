5a2f1b6ecabf0024b4ef14befb2270ddf9973956
ZONT_ <danya10001@gmail.com>
2020-12-04 20:19:25 +0300

    WIP, в принципе уже работает, но есть критический баг

---
+++ src/main/java/ru/zont/rgdsb/command/exec/Cmd.java
@@ -0,0 +1,38 @@
+package ru.zont.rgdsb.command.exec;
+
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import org.jetbrains.annotations.NotNull;
+import ru.zont.rgdsb.command.CommandAdapter;
+
+import java.util.Properties;
+
+import static ru.zont.rgdsb.tools.Strings.STR;
+
+public class Cmd extends CommandAdapter {
+    public Cmd() throws RegisterException { super(); }
+
+    @Override
+    public void onRequest(@NotNull MessageReceivedEvent event) throws UserInvalidArgumentException {
+
+    }
+
+    @Override
+    public String getCommandName() {
+        return "cmd";
+    }
+
+    @Override
+    public String getSynopsis() {
+        return "cmd <command>";
+    }
+
+    @Override
+    public String getDescription() {
+        return STR.getString("comm.cmd.desc");
+    }
+
+    @Override
+    protected Properties getPropsDefaults() {
+        return null;
+    }
+}
