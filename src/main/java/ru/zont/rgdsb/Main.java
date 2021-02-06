4d3cd7a51521b9ff6b06c24e4f11713353c69f1d
ZONT_ <danya10001@gmail.com>
2021-02-06 19:33:07 +0300

    Фиксы

--- src/main/java/ru/zont/rgdsb/Main.java
+++ src/main/java/ru/zont/rgdsb/Main.java
@@ -1,13 +1,23 @@
 package ru.zont.rgdsb;
 
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
-import ru.zont.dsbot.core.tools.Configs;
 import ru.zont.dsbot.core.ZDSBot;
+import ru.zont.dsbot.core.commands.CommandAdapter;
+import ru.zont.dsbot.core.handler.LStatusHandler;
+import ru.zont.dsbot.core.tools.Configs;
+import ru.zont.rgdsb.command.*;
+import ru.zont.rgdsb.command.exec.Cmd;
+import ru.zont.rgdsb.command.exec.Do;
+import ru.zont.rgdsb.command.exec.Exec;
+import ru.zont.rgdsb.command.exec.Term;
 import ru.zont.rgdsb.listeners.LPlayersMonitoring;
 import ru.zont.rgdsb.listeners.LServerStatus;
 import ru.zont.rgdsb.tools.Globals;
 
 import javax.security.auth.login.LoginException;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.Properties;
 
 public class Main extends ListenerAdapter {
@@ -27,10 +37,29 @@ public class Main extends ListenerAdapter {
             setProperty("TA_IDS", "331524458806247426");
         }});
 
-        final ZDSBot zdsBot = new ZDSBot(args[0], Globals.version, "ru.zont.rgdsb.command", "ru.zont.rgdsb.listeners");
+        final ZDSBot zdsBot = new ZDSBot(args[0], Globals.version, commands(), handlers());
         zdsBot.getJdaBuilder()
                 .addEventListeners(Globals.serverStatus = new LServerStatus())
                 .addEventListeners(new LPlayersMonitoring());
         zdsBot.create().awaitReady();
+    }
+
+    private static List<Class<? extends LStatusHandler>> handlers() {
+        return Collections.emptyList();
+    }
+
+    private static List<Class<? extends CommandAdapter>> commands() {
+        return Arrays.asList(
+                Cmd.class,
+                Do.class,
+                Exec.class,
+                Term.class,
+                Config.class,
+                GMs.class,
+                Help.class,
+                Ping.class,
+                Say.class,
+                Status.class
+        );
     }
 }
