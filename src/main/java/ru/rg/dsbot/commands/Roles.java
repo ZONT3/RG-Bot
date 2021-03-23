1978d3110d984b0dffb4bbd5705748a3c0dbd02f
ZONT_ <danya10001@gmail.com>
2021-03-23 07:40:08 +0300

    Улучшение ролей

--- src/main/java/ru/rg/dsbot/commands/Roles.java
+++ src/main/java/ru/rg/dsbot/commands/Roles.java
@@ -1,19 +1,24 @@
 package ru.rg.dsbot.commands;
 
-import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.Role;
 import ru.zont.dsbot2.NotImplementedException;
 import ru.zont.dsbot2.ZDSBot;
 import ru.zont.dsbot2.commands.CommandAdapter;
 import ru.zont.dsbot2.commands.Input;
 import ru.zont.dsbot2.commands.UserInvalidInputException;
 import ru.zont.dsbot2.tools.Commons;
+import ru.zont.dsbot2.tools.Data;
+import ru.zont.dsbot2.tools.ZDSBMessages;
 
 import java.util.*;
 
 import static ru.rg.dsbot.tools.TRoles.*;
-import static ru.zont.dsbot2.tools.ZDSBStrings.*;
+import static ru.zont.dsbot2.tools.ZDSBStrings.STR;
 
 public class Roles extends CommandAdapter {
+
+    private final Data<HashMap<Long, HashSet<Integer>>> allowance = new Data<>("roles-allowance");
 
     public Roles(ZDSBot.GuildContext context) {
         super(context);
@@ -21,12 +26,13 @@ public class Roles extends CommandAdapter {
 
     @Override
     public void onCall(Input input) {
-        final MessageReceivedEvent event = input.getEvent();
         Commons.rolesLikeRouter(0, this::set, this::rm, this::get)
                 .setError(STR.getString("comms.gms.err.first_arg"))
                 .addCase((i) ->
                         Commons.rolesLikeRouter(1, this::autoSet, this::autoRm, this::autoGet)
                                 .acceptInput(i), "auto")
+                .addCase(this::allow, "allow")
+                .addCase(this::deny, "deny")
                 .acceptInput(input);
     }
 
@@ -43,7 +49,7 @@ public class Roles extends CommandAdapter {
     }
 
     private Profile fetchProfile(String argDisID, String argSteamID) {
-        long userid = argDisID != null ? Commons.userMentionToID(argDisID) : -1;
+        long userid = argDisID != null ? Commons.mentionToID(argDisID) : -1;
 
         Profile profile;
         String uid;
@@ -64,9 +70,46 @@ public class Roles extends CommandAdapter {
         return new Profile(uid, userid, roles);
     }
 
+    private void allow(Input input) {
+        input.assertArgCount(3, false);
+        long dsRoleID = Commons.mentionToID(input.getArg(1, false));
+        String idArg = input.getArg(2, false);
+        if (!idArg.matches("\\d+"))
+            throw new UserInvalidInputException("comms.roles.err.arg3");
+        int id = Integer.parseInt(idArg);
+
+        allowance.op(new HashMap<>(), map -> {
+            HashSet<Integer> set = map.getOrDefault(dsRoleID, new HashSet<>());
+            set.add(id);
+            map.put(dsRoleID, set);
+        });
+        ZDSBMessages.addOK(input.getMessage());
+    }
+
+    private void deny(Input input) {
+        input.assertArgCount(2, false);
+        long dsRoleID = Commons.mentionToID(input.getArg(1, false));
+        String idArg = input.getArg(2, false);
+        Integer id;
+        if (idArg != null && idArg.matches("\\d+"))
+            id = Integer.valueOf(idArg);
+        else id = null;
+
+        allowance.op(new HashMap<>(), map -> {
+            if (id == null)
+                map.remove(dsRoleID);
+            else {
+                HashSet<Integer> set = map.getOrDefault(dsRoleID, new HashSet<>());
+                set.remove(id);
+                map.put(dsRoleID, set);
+            }
+        });
+        ZDSBMessages.addOK(input.getMessage());
+    }
+
     private void set(Input input) {
         input.assertArgCount(3);
-        final List<String> args = Arrays.asList(input.getArgs());
+        final List<String> args = Arrays.asList(input.getArgs(false));
         int id = parseID(args.get(1));
         final Profile profile = fetchProfile(args.get(2), args.size() >= 4 ? args.get(3) : null);
         profile.roles.add(id);
@@ -75,13 +118,13 @@ public class Roles extends CommandAdapter {
     }
 
     private void rm(Input input) {
-        input.assertArgCount(3);
-        List<String> args = Arrays.asList(input.getArgs());
+        input.assertArgCount(3, false);
+        List<String> args = Arrays.asList(input.getArgs(false));
         int id = parseID(args.get(1));
 
         final String arg = args.get(2);
         long userid = -1; String steamid = null;
-        try { userid = Commons.userMentionToID(arg); }
+        try { userid = Commons.mentionToID(arg); }
         catch (UserInvalidInputException ignored) { }
         if (userid < 0) {
             try {
@@ -98,14 +141,14 @@ public class Roles extends CommandAdapter {
     }
 
     private void get(Input input) {
-        List<String> args = Arrays.asList(input.getArgs());
+        List<String> args = Arrays.asList(input.getArgs(false));
         if (args.size() < 2) {
             input.getChannel().sendMessage(msgList()).queue();
             return;
         }
         final String arg = args.get(1);
         long userid = -1; String steamid = null; int id = 0;
-        try { userid = Commons.userMentionToID(arg); }
+        try { userid = Commons.mentionToID(arg); }
         catch (UserInvalidInputException ignored) { }
         if (userid < 0) {
             try {
@@ -150,14 +193,17 @@ public class Roles extends CommandAdapter {
 
     @Override
     public String getSynopsis() {
-        return "roles add|set|get|list|rm|del|auto ...\n" +
-                "roles add|set <ID> <@who> [steamID64]\n" +
-                "roles rm|del <ID> <@who|steamID64>\n" +
-                "roles get|list [ID|@who]\n" +
-                "roles auto add|set|rm|del|list|get ...\n" +
-                "roles auto add|set <@ds-role|ds-role-id> <ID>\n" +
-                "roles auto rm|del <ID|@ds-role|ds-role-id>\n" +
-                "roles auto list|get";
+        return """
+                roles add|set|get|list|rm|del|allow|deny|auto ...
+                roles add|set <ID> <@who> [steamID64]
+                roles rm|del <ID> <@who|steamID64>
+                roles get|list [ID|@who]
+                roles allow <@role> <ID>
+                roles deny <@role> [ID]
+                roles auto add|set|rm|del|list|get ...
+                roles auto add|set <@ds-role|ds-role-id> <ID>
+                roles auto rm|del <ID|@ds-role|ds-role-id>
+                roles auto list|get""";
     }
 
     @Override
@@ -168,7 +214,31 @@ public class Roles extends CommandAdapter {
 
     @Override
     public boolean checkPermission(Input input) {
-        return Commons.rolesLikePermissions(input, Arrays.asList("set", "rm", "add", "del", "auto"));
+        if (Commons.rolesLikePermissions(input, Arrays.asList("set", "rm", "add", "del", "auto", "allow", "deny")))
+            return true;
+
+        Member member = input.getMember();
+        String arg1 = input.getArg(1);
+
+        boolean check = member != null &&
+                input.argEquals(0, "set","rm","add","del") &&
+                arg1 != null &&
+                arg1.matches("\\d+");
+
+
+        return check && allowance.op(new HashMap<>(), arg -> {
+
+            int id = Integer.parseInt(arg1);
+            List<Role> roles = member.getRoles();
+            for (Map.Entry<Long, HashSet<Integer>> entry: arg.entrySet()) {
+                Role role = roles.stream().filter(r -> r.getIdLong() == entry.getKey()).findAny().orElse(null);
+                if (role == null) continue;
+                if (entry.getValue().contains(id))
+                    return true;
+            }
+
+            return false;
+        });
     }
 
     @Override
