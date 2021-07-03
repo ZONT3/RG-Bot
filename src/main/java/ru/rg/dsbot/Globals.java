65d10c8426f5abb36409cd9c8d86e0dde9030a48
ZONT_ <danya10001@gmail.com>
2021-07-04 00:43:28 +0300

    Медиа

--- src/main/java/ru/rg/dsbot/Globals.java
+++ src/main/java/ru/rg/dsbot/Globals.java
@@ -3,4 +3,9 @@ package ru.rg.dsbot;
 public class Globals {
     public static String dbConnection;
     public static String gamePort = "2302";
+
+    public static final String TWITCH_API_CLIENT_ID = "5n48e9ffucc1movvu6who9n4wq63u3";
+    public static String TWITCH_API_SECRET;
+
+    public static String GOOGLE_API;
 }
--- src/main/java/ru/rg/dsbot/Main.java
+++ src/main/java/ru/rg/dsbot/Main.java
@@ -1,9 +1,11 @@
 package ru.rg.dsbot;
 
 import ru.rg.dsbot.commands.GMs;
+import ru.rg.dsbot.commands.Media;
 import ru.rg.dsbot.commands.Roles;
 import ru.rg.dsbot.listeners.LPlayersMonitoring;
 import ru.rg.dsbot.listeners.LServerStatus;
+import ru.rg.dsbot.loops.LMedia;
 import ru.zont.dsbot2.ZDSBot;
 import ru.zont.dsbot2.ZDSBotBuilder;
 import ru.zont.dsbot2.commands.implement.Help;
@@ -29,8 +31,8 @@ public class Main {
     }
 
     public static class Config extends ru.zont.dsbot2.Config {
-        public final Entry myChannel = new Entry("792009238062039070");
-        public final Entry myRole = new Entry("777555228");
+        public final Entry channel_streams = new Entry("860993130765352980");
+        public final Entry channel_video = new Entry("860993130765352980");
 
         public Config() {
             super.prefix = new Entry("//");
@@ -48,14 +50,18 @@ public class Main {
                 .setConfig(new Config())
                 .addCommands(Help.class, Say.class,
                         Exec.class, Cmd.class, Do.class, Term.class,
-                        Roles.class, GMs.class
+                        Roles.class, GMs.class, Media.class
                 )
+                .addLoops(LMedia.class)
                 .addListeners(new LServerStatus(), new LPlayersMonitoring())
                 .build();
     }
 
     private static void checkArgs(String[] args) throws LoginException {
-        if (args.length < 2) throw new LoginException("API token and/or DB connection not provided!");
+        if (args.length < 4) throw new LoginException("API token and/or DB connection not provided!");
         Globals.dbConnection = args[1];
+
+        Globals.TWITCH_API_SECRET = args[2];
+        Globals.GOOGLE_API = args[3];
     }
 }
